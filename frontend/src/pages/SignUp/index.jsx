c2435eee2eebe255bab8472af5126e84db921dc9
hasannazimakyol <hnakyol@hotmail.com>
2023-09-25 16:00:17 +0300

    signup page is added

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
+
 export function SignUp() {
-    return <h1>Sign Up</h1>
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordRepeat, setPasswordRepeat] = useState();
+
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">E-mail</label>
+        <input
+          id="email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <label htmlFor="passwordRepeat">Password Repeat</label>
+        <input
+          id="passwordRepeat"
+          type="password"
+          onChange={(event) => {
+            setPasswordRepeat(event.target.value);
+          }}
+        />
+      </div>
+      <button disabled={!password || password !== passwordRepeat}>Sign Up</button>
+    </>
+  );
 }
